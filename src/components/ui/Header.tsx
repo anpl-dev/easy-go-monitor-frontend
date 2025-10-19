6e96c175a7e82138bf0476e180fcacd706871cf6
asuma <anpl.github@gmail.com>
2025-10-19 13:42:34 +0900

    small fix

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -3,7 +3,7 @@ import { cn } from "../../lib/utils";
 import { Button } from "./Button";
 
 const header = tv({
-  base: "flex items-center justify-between w-full px-6 py-8 bg-white",
+  base: "flex items-center justify-between w-full px-6 py-7 bg-white",
 });
 
 type HeaderProps = {
@@ -18,7 +18,7 @@ export function Header({ title = "Dashboard" }: HeaderProps) {
   return (
     <header className={cn(header())}>
       {/* 左側: ページタイトル */}
-      <div className="flex-col items-center gap-y-3">
+      <div className="flex-col items-center gap-y-2">
         <h1 className="text-blue-600 font-bold mb-4">Easy Go Monitor</h1>
         <h1 className="text-2xl font-semibold text-gray-800">{title}</h1>
       </div>
